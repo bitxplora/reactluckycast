3b29c90fc121ce445e3be173a99ebf963206f967
Dayo Olutayo <olutayo@olutayo.dev>
2025-01-23 23:50:59 +0100

    Added ten dice to the LuckyCast component using Die component. This laid the foundation for LuckyCast game.

--- src/components/LuckyCast.jsx
+++ src/components/LuckyCast.jsx
@@ -1,7 +1,15 @@
 import Die from "./Die";
+import { nanoid } from "nanoid";
 
 export default function LuckyCast() {
+
+  const Dice = Array.from(Array(10), () => {
+    return <Die key={nanoid()} value={6} isFreezed={false} />
+  })
+
   return (
-    <Die value={6} isFreezed={false} />
+    <div className="luckycast">
+      {Dice} 
+    </div>
   );
 }
