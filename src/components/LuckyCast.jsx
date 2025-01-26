881f369cbaf94c2a13a2f6926f0b8401a7c944b6
Dayo Olutayo <olutayo@olutayo.dev>
2025-01-26 23:43:37 +0100

    Added function that generates the die value, that is, 1-6 for each die.

--- src/components/LuckyCast.jsx
+++ src/components/LuckyCast.jsx
@@ -1,10 +1,16 @@
+import { useState } from 'react';
+
 import Die from "./Die";
 import { nanoid } from "nanoid";
 
 export default function LuckyCast() {
 
+  function diceNumber() {
+    return Math.ceil(Math.random() * 6);
+  }
+
   const Dice = Array.from(Array(10), () => {
-    return <Die key={nanoid()} value={6} isFreezed={false} />
+    return <Die key={nanoid()} value={diceNumber()} isFreezed={false} />
   })
 
   return (
