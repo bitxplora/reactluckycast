ff7f7e65055450a572594be48e824c6e4d2c77e6
Dayo Olutayo <olutayo@olutayo.dev>
2025-01-29 20:58:16 +0100

    Refactored initial dice data into a function dataFactory

--- src/components/LuckyCast.jsx
+++ src/components/LuckyCast.jsx
@@ -5,30 +5,40 @@ import { nanoid } from "nanoid";
 
 export default function LuckyCast() {
 
+  // let isRoll = true;
+  const [ isRoll, setIsRoll ] = useState(true);
+
   function diceNumber() {
     return Math.ceil(Math.random() * 6);
   }
 
-  const data = Array.from(Array(10), ()=> {
+  function dataFactory() {
+   return Array.from(Array(10), ()=> {
     return {
       id: nanoid(),
       isFreezed: false,
       value: diceNumber(),
-    }
-  })
+      }
+    })
+  }
 
-  const [ diceData, setDiceData ] = useState(data);
+  const [ diceData, setDiceData ] = useState(dataFactory());
+  // console.log(diceData);
 
   const Dice = Array.from(diceData, (data) => {
     return <Die key={data.id} value={data.value} isFreezed={data.isFreezed} />
   })
+
+  function onRoll() {
+    setIsRoll(!isRoll);
+  }
 
   return (
     <div>
       <div className="luckycast">
         {Dice}
       </div>
-      <button>Roll</button>
+      <button onClick={onRoll} className="rollGame">{ isRoll ? "Roll" : "New Game" }</button>
     </div>
   );
 }
